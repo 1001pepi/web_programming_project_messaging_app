57b91c82610af4efd5e1e17d875bfab50b6797fb
ralietonle <jovialetonle@gmail.com>
2020-01-01 12:53:39 +0100

    Second commit, add index page and four-oh-four page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { AppComponent } from './app.component';
 import { PreloadAllModules } from '@angular/router';
 import { AuthGuardService } from './auth/services/auth-guard.service';
 import { IndexComponent } from './index/index.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 const routes: Routes = [
   
   { path: '', component: IndexComponent },
   /* Feature modules */
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'core', /*canActivate: [AuthGuardService],*/ loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }
-  /*{ path:'**', component:AppComponent}*/
+  { path: 'core', /*canActivate: [AuthGuardService],*/ loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
