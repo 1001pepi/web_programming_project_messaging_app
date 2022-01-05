7f7ebe40d365e78d89a47417ec43ad59c8294f5b
ralietonle <jovialetonle@gmail.com>
2022-01-05 12:34:08 +0100

    Fourth commit, add modification contact functionalities

--- src/app/core/contacts-list-item/contacts-list-item.component.ts
+++ src/app/core/contacts-list-item/contacts-list-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Contact } from 'src/app/models/contact.model';
+import { ContactService } from '../services/contact.service';
 @Component({
   selector: 'app-contacts-list-item',
   templateUrl: './contacts-list-item.component.html',
@@ -10,9 +12,16 @@ export class ContactsListItemComponent implements OnInit {
   @Input() name:string = "John Doe";
   @Input() number:string = "6700000876";
   @Input() photo:string = '';
-  constructor() { }
+  currentContact:Contact = new Contact("675654454","Joe","");
+  constructor(private contactService:ContactService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(form: NgForm) {
+    const name = form.value['name'];
+    const number = form.value['number'];
+    const photo = form.value['photo'];
+    this.contactService.modifyContact(this.currentContact,name,number, photo);
+    form.resetForm();
+  }
 }
